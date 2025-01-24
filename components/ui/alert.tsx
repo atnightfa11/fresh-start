b91893322124c60f3deb37e12fa7afd6edef3c17
atnightfa11 <devoid_quiver0n@icloud.com>
2025-01-24 11:24:46 -0600

    Fix utils import paths in UI components

--- components/ui/alert.tsx
+++ components/ui/alert.tsx
@@ -1,3 +1,6 @@
+import * as React from "react"
+import { cva, type VariantProps } from "class-variance-authority"
+import { cn } from "../../lib/utils"
 import { ReactNode } from 'react';
 
 interface AlertProps {
--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -1,4 +1,6 @@
+import * as React from "react"
 import { ReactNode } from 'react';
+import { cn } from "../../lib/utils"
 
 interface CardProps {
   children: ReactNode;
